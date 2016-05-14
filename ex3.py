6a7faf3d457ed8f3b84ee5faefce5dbc97dceb07
mittortz <mittortz@gmail.com>
2016-05-14 00:17:42 -0400

    First commit

---
+++ ex3.py
@@ -0,0 +1,30 @@
+# Announce purpose of script
+print "I will now count my chickens:"
+
+# Count hens and roosters
+print "Hens", 25 + 30 / 6.0
+print "Roosters" , 100 - 25 * 3.0 % 4
+
+# Count eggs
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4.0 % 2 - 1 / 4 + 6
+
+# Announce inequality question
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Test inequality
+print 3 + 2 < 5 - 7
+
+# Break down inequality into separate equations
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+# Perform more inequality tests
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
