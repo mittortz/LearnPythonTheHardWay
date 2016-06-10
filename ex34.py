fd75f410dff5967072d5fb594ada7feeb5ed46ae
mittortz <mittortz@gmail.com>
2016-06-10 17:25:24 -0400

    Complete ex. 34-36

---
+++ ex34.py
@@ -0,0 +1,5 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at 1 is python?"
+print animals[1]
+print "The third animal is peacock, position 2"
---
+++ ex36if.py
@@ -0,0 +1,6 @@
+test = raw_input("Are you a jerk?")
+
+if test == 'Y':
+    print "Fuck you."
+
+print "I love you."
